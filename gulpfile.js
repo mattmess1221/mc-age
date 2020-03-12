75f8d188365dbacb118ffd58e724c6e5ccc5150e
Matthew Messinger <mattmess1221@gmail.com>
2020-03-11 22:05:25 -0400

    Add a webserver to gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const clean = require('gulp-clean');
 const ts = require('gulp-typescript');
 const minify = require('gulp-minify');
 const sourcemaps = require('gulp-sourcemaps');
+const webserver = require('gulp-webserver');
 
 let tsProject = ts.createProject('tsconfig.json');
 
@@ -15,22 +16,39 @@ function cleanPublic() {
 
 function buildTypescript() {
     return tsProject.src()
-        .pipe(gulp.dest(publicFolder + "/js"))
         .pipe(sourcemaps.init())
         .pipe(tsProject())
         .pipe(minify({
             ext: {min: '.min.js'}
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(publicFolder + "/js"));
-}
-
-function copyResources() {
-    return gulp.src('./res/**/*')
         .pipe(gulp.dest(publicFolder));
 }
 
-module.exports.cleanPublic = cleanPublic;
-module.exports.buildTypescript = buildTypescript;
-module.exports.copyResources = copyResources;
-module.exports.default = gulp.series(this.cleanPublic, gulp.parallel(this.copyResources, this.buildTypescript));
+function copyResources() {
+    return gulp.src(['./res/**/*'])
+        .pipe(gulp.dest(publicFolder));
+}
+
+const defaultTask = gulp.series(cleanPublic, gulp.parallel(copyResources, buildTypescript));
+
+function watch() {
+    return gulp.watch(["src/", "res/"], defaultTask);
+}
+
+function serve() {
+    return gulp.src(publicFolder)
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: true
+        }))
+}
+
+module.exports = {
+    cleanPublic: cleanPublic,
+    buildTypescript: buildTypescript,
+    copyResources: copyResources,
+    serve: gulp.series(defaultTask, gulp.parallel(watch, serve)),
+    default: defaultTask
+};
